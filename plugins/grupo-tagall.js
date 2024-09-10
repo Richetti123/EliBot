a4cc30108817c3a1fb9727919b5475d5e2bafbea
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-09-11 01:23:00 +0200

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -5,7 +5,7 @@ throw false
 }
 let pesan = args.join` `
 let oi = `@dzn.eliza ${pesan}`
-let teks = `*𝙰𝙽𝙾𝚃𝙴𝙽𝚂𝙴 𝙿𝙻𝙰𝙽𝚃𝙰𝚂\n${oi}\n\n*𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨:*\n`
+let teks = `*𝙰𝙽𝙾𝚃𝙴𝙽𝚂𝙴 𝙿𝙻𝙰𝙽𝚃𝙰𝚂*\n${oi}\n\n*𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨:*\n`
 for (let mem of participants) {
 teks += `🍄 @${mem.id.split('@')[0]}\n`}
 teks += `EliBot`
